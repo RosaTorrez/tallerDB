01cc46bb5ae53ab09ae208e34761be6ca6d084ed
CristianGallardo <cristkeving679@gmail.com>
2024-07-01 22:45:24 -0400

    xd

---
+++ src/app/dashboard/create/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function createPage() {
+  return (
+    <div className="hola">
+        <h1>Formulario de Ejemplo para TCX</h1>
+        <form action="/generate-tcx" method="post">
+        
+        </form>
+    </div>
+    
+  )
+}
+
+export default createPage
