31c071d7bced6bb0c312c62853be28c4282fcdc2
CristianGallardo <cristkeving679@gmail.com>
2024-07-03 11:35:22 -0400

    acabando el crud

--- scripts/seed.js
+++ scripts/seed.js
@@ -23,25 +23,14 @@ async function seedActor() {
   id_actor SERIAL PRIMARY KEY,
   nombre VARCHAR(50) NOT NULL,
   apellido VARCHAR(50) NOT NULL,
-  ultima_actualizacion TIMESTAMP
+  ultima_actualizacion TIMESTAMP NOT NULL
   );
   `;
 
     console.log(`Created "invoices" table`);
 
     // Insert data into the "invoices" table
-    const insertedInvoices = await Promise.all(
-      invoices.map(
-        (invoice) => client.sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-      ON CONFLICT (id) DO NOTHING;
-    `,
-      ),
-    );
-
-    console.log(`Seeded ${insertedInvoices.length} invoices`);
-
+    
     return {
       createTable,
       invoices: insertedInvoices,
@@ -58,30 +47,15 @@ async function seedCategoria(categoria) {
     const createTable = await categoria.sql`
     CREATE TABLE IF NOT EXISTS categoria (
       id_categoria INT PRIMARY KEY
-      nombre VARCHAR(50) NOT NULL,
-      ultima_actualizacion TIMESTAMP
+      nombre VARCHAR(50) NOT NULL UNIQUE,
+      ultima_actualizacion TIMESTAMP NOT NULL
     );
   `;
 
     console.log(`Created "customers" table`);
 
     // Insert data into the "customers" table
-    const insertedCustomers = await Promise.all(
-      customers.map(
-        (customer) => client.sql`
-      INSERT INTO customers (id, name, email, image_url)
-      VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-      ON CONFLICT (id) DO NOTHING;
-    `,
-      ),
-    );
-
-    console.log(`Seeded ${insertedCustomers.length} customers`);
-
-    return {
-      createTable,
-      customers: insertedCustomers,
-    };
+    
   } catch (error) {
     console.error("Error seeding customers:", error);
     throw error;
@@ -95,9 +69,9 @@ async function seedCliente(cliente) {
           id_cliente SERIAL PRIMARY KEY,
           nombre VARCHAR(50) NOT NULL,
           apellido VARCHAR(50) NOT NULL,
-          correo_electronico VARCHAR(50) NOT NULL,
+          correo_electronico VARCHAR(50) NOT NULL UNIQUE,
           activo BOOLEAN NOT NULL,
-          telefono INT
+          telefono INT UNIQUE
       );
     `;
     return {
@@ -114,15 +88,15 @@ async function seedPelicula(pelicula) {
     const createTable = await pelicula.sql`
   CREATE TABLE pelicula (
     id_pelicula SERIAL PRIMARY KEY,
-    titulo VARCHAR(50) NOT NULL,
-    descripcion VARCHAR(50),
+    titulo VARCHAR(50) NOT NULL UNIQUE,
+    descripcion VARCHAR(50) UNIQUE,
     anio_estreno SMALLINT NOT NULL,
     id_idioma SMALLINT,
-    id_idioma_original SMALLINT ,
-    duracion_alquiler SMALLINT NOT NULL,
-    tarifa_alquiler MONEY NOT NULL,
-    duracion INTERVAL NOT NULL,
-    costo_reemplazo MONEY NOT NULL,
+    id_idioma_original SMALLINT NOT NULL,
+    duracion_alquiler SMALLINT NOT NULL CHECK (duracion_alquiler > 0),
+    tarifa_alquiler MONEY NOT NULL CHECK (tarifa_alquiler > 0),
+    duracion INTERVAL NOT NULL CHECK (duracion > ‘00:30:00’),
+    costo_reemplazo MONEY NOT NULL CHECK (costo_reemplazo > 0),
     clasificacion VARCHAR(50),
     ultima_actualizacion TIMESTAMP NOT NULL,
     FOREIGN KEY (id_idioma) REFERENCES idioma(id_idioma)
@@ -147,9 +121,9 @@ async function seedActor_Pelicula(actor_pelicula) {
     // Create the "revenue" table if it doesn't exist
     const createTable = await actor_pelicula.sql`
     CREATE TABLE IF NOT EXISTS actor_pelicula (
-      id_actor INT,
-      id_pelicula INT,
-      ultima_actualizacion TIMESTAMP,
+      id_actor INT NOT NULL,
+      id_pelicula INT NOT NULL,
+      ultima_actualizacion TIMESTAMP NOT NULL,
       FOREIGN KEY (id_actor) REFERENCES actor(id_actor)
       ON UPDATE CASCADE
       ON DELETE CASCADE,
@@ -189,9 +163,9 @@ async function seedCategoria_Pelicula(categoria_pelicula) {
     // Create the "categoria_pelicula" table if it doesn't exist
     const createTable = await categoria_pelicula.sql`
     CREATE TABLE IF NOT EXISTS actor_pelicula (
-      id_pelicula INT,
-      id_categoria INT,
-      ultima_actualizacion TIMESTAMP,
+      id_pelicula INT NOT NULL,
+      id_categoria INT NOT NULL,
+      ultima_actualizacion TIMESTAMP NOT NULL,
       FOREIGN KEY (id_pelicula) REFERENCES pelicula(id_pelicula)
       ON UPDATE CASCADE
       ON DELETE CASCADE,
@@ -232,8 +206,8 @@ async function seedInventario(inventario) {
     const createTable = await inventario.sql`
     CREATE TABLE IF NOT EXISTS inventario (
       id_inventario SERIAL PRIMARY KEY,
-      id_pelicula INT,
-      ultima_actualizacion TIMESTAMP,
+      id_pelicula INT NOT NULL,
+      ultima_actualizacion TIMESTAMP NOT NULL,
       FOREIGN KEY (id_pelicula) REFERENCES pelicula(id_pelicula)
       ON UPDATE CASCADE
       ON DELETE CASCADE 
@@ -271,8 +245,8 @@ async function seedIdioma(idioma) {
     const createTable = await idioma.sql`
   CREATE TABLE idioma (
     id_idioma SERIAL PRIMARY KEY,
-    nombre_idioma VARCHAR(20) NOT NULL,
-    ultima_actualizacion TIMESTAMP
+    nombre_idioma VARCHAR(20) NOT NULL UNIQUE,
+    ultima_actualizacion TIMESTAMP NOT NULL
     );
     `;
     return {
@@ -289,10 +263,10 @@ async function seedRenta(renta) {
     const createTable = await renta.sql`
   CREATE TABLE renta (
     id_renta SERIAL PRIMARY KEY,
-    fecha_renta TIMESTAMP,
-    id_cliente SMALLINT,
-    fecha_pago TIMESTAMP,
-    ultima_actualizacion TIMESTAMP,
+    fecha_renta TIMESTAMP NOT NULL,
+    id_cliente SMALLINT NOT NULL,
+    fecha_pago TIMESTAMP NOT NULL,
+    ultima_actualizacion TIMESTAMP NOT NULL,
     FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
     ON UPDATE CASCADE
     ON DELETE CASCADE
