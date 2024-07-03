eee8b918a32e61833e02a29b4401051fdddb4541
CristianGallardo <cristkeving679@gmail.com>
2024-07-03 16:25:40 -0400

    FIIIiN

--- src/app/dashboard/categories/listCategories/page.tsx
+++ src/app/dashboard/categories/listCategories/page.tsx
@@ -1,36 +1,82 @@
-import { lusitana } from "@/app/ui/fonts";
-import { CreateCategories } from "@/app/ui/categories/buttons";
-import Search from "@/app/ui/search";
-import { Suspense } from "react";
-import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
-import Table from "@/app/ui/client/table";
-import Pagination from "@/app/ui/invoices/pagination";
-import { fetchCategoriesPages } from "@/app/lib/queries";
+import {fetchCategories} from "@/app/lib/queries";
+import {Card, Table, TableBody, TableCell, TableHead, TableHeaderCell, TableRow} from "@tremor/react";
+import {lusitana} from "@/app/ui/fonts";
+import {PlusIcon} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import {DeleteCategory, UpdateCategory} from "@/app/ui/categories/buttons";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: { query?: string; page?: number };
-}) {
-  const currentPage = Number(searchParams?.page!) || 1;
-  const query = searchParams?.query || "";
-  const totalPages = await fetchCategoriesPages(query);
+export default async function Page() {
+    const categories = await fetchCategories()
+    function formatDateTime(dateTimeString: string) {
+        const date = new Date(dateTimeString);
 
+        // Formatear la fecha y hora
+        const formattedDate = date.toLocaleDateString('es-ES', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'
+        });
+
+        const formattedTime = date.toLocaleTimeString('es-ES', {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        });
+
+        // Devolver la fecha y hora formateada
+        return `${formattedDate} ${formattedTime}`;
+    }
   return (
-    <div className="w-full">
-      <div className="flex w-full items-center justify-center">
-        <h1 className={`${lusitana.className} text-2xl`}>Categories</h1>
-      </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder={"Search Invoice..."} />
-        <CreateCategories />
-      </div>
-      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense>
-      <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
-      </div>
+    <div>
+      <Card className={"flex flex-col gap-5"}>
+          <Card className={"flex justify-between items-center"}>
+              <h1 className={`${lusitana.className}`}>
+                  Lista de Categorias
+              </h1>
+              <Link
+                  href="/dashboard/categories/createCategories"
+                  className="flex h-full items-center rounded-lg bg-blue-600 p-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              >
+                  <span className="hidden md:block">
+                      Crear Categoria
+                  </span>{" "}
+                  <PlusIcon className="h-5 md:ml-4" />
+              </Link>
+          </Card>
+          <Card>
+              <Table>
+                  <TableHead>
+                      <TableRow>
+                          <TableHeaderCell>
+                              Nombre
+                          </TableHeaderCell>
+                          <TableHeaderCell>
+                              Ultima Actualizacion
+                          </TableHeaderCell>
+                          <TableHeaderCell>
+                              Acciones
+                          </TableHeaderCell>
+                      </TableRow>
+                  </TableHead>
+                  <TableBody>
+                      {categories.map(categoria =>(
+                          <TableRow key={categoria.id_categoria}>
+                              <TableCell>
+                                  {categoria.nombre}
+                              </TableCell>
+                              <TableCell>
+                                  {formatDateTime(categoria.ultima_actualizacion.toString())}
+                              </TableCell>
+                              <TableCell className={"flex gap-5"}>
+                                  <UpdateCategory id={categoria.id_categoria}/>
+                                  <DeleteCategory id={categoria.id_categoria}/>
+                              </TableCell>
+                          </TableRow>
+                      ))}
+                  </TableBody>
+              </Table>
+          </Card>
+      </Card>
     </div>
   );
 }
